8b04fc76640dfd05534a66bdf16424926a6b7638
Kristin Agcaoili <kagcaoil@ucsd.edu>
2017-02-15 21:32:12 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,12 +27,36 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, goProject);
+}
+
+function goProject(result) {
+	console.log(result);
+	var id = result.id;
+
+	var htmlString = '<img src="' + result['image'] + '" class="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p>' + '<p>' + result['summary'] + '</p>';
+
+	//$("#project" + id + " .details").html("<p>" + result['summary'] + "</p>");
+	$("#project" + id + " .details").html(htmlString);
 }
 
 /*
- * Make an AJAX call to retrieve a color palette for the site
+ * resul an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", paletteChange);
+}
+
+function paletteChange(result) {
+	var colors = result.colors.hex;
+	console.log("colors is: " + colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
